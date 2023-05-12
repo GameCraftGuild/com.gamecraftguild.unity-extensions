9e73ecd269806afc4c2c6069bbd81474204db196
jthpotz <jthpotz@gmail.com>
2023-05-11 18:05:21 -0700

    Modified GetRandomFromWeightedList to use two seperate lists

--- Packages/com.gamecraftguild.unity-extensions/Runtime/IList/IListExtensions.cs
+++ Packages/com.gamecraftguild.unity-extensions/Runtime/IList/IListExtensions.cs
@@ -25,33 +25,34 @@ namespace GameCraftGuild.UnityExtensions.IList {
         }
 
         /// <summary>
-        /// Randomly select an object from <paramref name="list" /> using provided weights for each object. An object with a nonpositive weight (weight &lt;= 0) will not be selected.
+        /// Randomly select an object from <paramref name="objects" /> using the weights in <paramref name="weights" /> given for each object. An object with a nonpositive weight (weight &lt;= 0) will not be selected.
         /// </summary>
         /// <remarks>
-        /// The second type for the KeyValuePair should be changed from "int" to "INumerics" once Unity supports .NET 7 and generic math.
+        /// The type for <paramref name="weights" /> should be changed from "int" to "INumerics" once Unity supports .NET 7 and generic math.
         /// </remarks>
-        /// <param name="list">List of KeyValuePairs where the Key is the object and the Value is the object's weight.</param>
-        /// <typeparam name="T">Type of objects in <paramref name="list" />.</typeparam>
-        /// <returns>Selected object from <paramref name="list" /> or default(<typeparamref name="T" />) if <paramref name="list" /> is null, empty, or all objects have nonpositive weight (weight &lt;= 0).</returns>
-        public static T GetRandomFromWeightedList<T> (this IList<KeyValuePair<T, int>> list) {
-            if (list == null || list.Count == 0) return default(T);
-            if (list.Count == 1) return (list[0].Value > 0 ? list[0].Key : default(T));
+        /// <param name="objects">List of objects of type :<typeparamref name="T" />.</param>
+        /// <param name="weights">Weights for each object.</param>
+        /// <typeparam name="T">Type of objects in <paramref name="objects" />.</typeparam>
+        /// <returns>Selected object from <paramref name="objects" /> or default(<typeparamref name="T" />) if <paramref name="objects" /> or <paramref name="weights" /> is null or empty, the list lengths do not match, or all objects have nonpositive weight (weight &lt;= 0).</returns>
+        public static T GetRandomFromWeightedList<T> (this IList<T> objects, IList<int> weights) {
+            if (objects == null || weights == null || objects.Count == 0 || weights.Count == 0 || objects.Count != weights.Count) return default(T);
+            if (objects.Count == 1) return (weights[0] > 0 ? objects[0] : default(T));
 
             int totalWeight = 0;
-            for (int index = 0; index < list.Count; index++) {
-                totalWeight += list[index].Value;
+            for (int index = 0; index < weights.Count; index++) {
+                totalWeight += weights[index];
             }
 
             int resultValue = Random.Range(0, totalWeight);
             int resultIndex = 0;
 
-            while (resultIndex < list.Count && resultValue >= list[resultIndex].Value) {
-                resultValue -= list[resultIndex].Value;
+            while (resultIndex < objects.Count && resultValue >= weights[resultIndex]) {
+                resultValue -= weights[resultIndex];
                 resultIndex++;
             }
 
-            if (resultIndex >= list.Count) return default(T);
-            return list[resultIndex].Key;
+            if (resultIndex >= objects.Count) return default(T);
+            return objects[resultIndex];
         }
     }
 }
