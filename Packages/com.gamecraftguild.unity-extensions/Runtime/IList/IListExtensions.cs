010aecb478bc23dffb5da8bd3ad83e40f6b3e620
mitch <mitchkm21@gmail.com>
2023-06-30 15:15:53 -0700

    slightly renames IListExtension methods

--- Packages/com.gamecraftguild.unity-extensions/Runtime/IList/IListExtensions.cs
+++ Packages/com.gamecraftguild.unity-extensions/Runtime/IList/IListExtensions.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 using Random = UnityEngine.Random;
 
@@ -12,12 +10,12 @@ namespace GameCraftGuild.UnityExtensions.IList {
     public static class IListExtensions {
 
         /// <summary>
-        /// Randomly select an object from <paramref name="list" />.
+        /// Randomly select an item from <paramref name="list" />.
         /// </summary>
-        /// <param name="list">List of objects to choose from.</param>
-        /// <typeparam name="T">Type of object in <paramref name="list" />.</typeparam>
-        /// <returns>Selected object from <paramref name="list" /> or default(<typeparamref name="T" />) if <paramref name="list" /> is null or empty.</returns>
-        public static T GetRandomFromList<T> (this IList<T> list) {
+        /// <param name="list">List of items to choose from.</param>
+        /// <typeparam name="T">Type of item in <paramref name="list" />.</typeparam>
+        /// <returns>Selected item from <paramref name="list" /> or default(<typeparamref name="T" />) if <paramref name="list" /> is null or empty.</returns>
+        public static T GetRandomItem<T> (this IList<T> list) {
             if (list == null || list.Count == 0) return default(T);
             if (list.Count == 1) return list[0];
 
@@ -25,18 +23,18 @@ namespace GameCraftGuild.UnityExtensions.IList {
         }
 
         /// <summary>
-        /// Randomly select an object from <paramref name="objects" /> using the weights in <paramref name="weights" /> given for each object. An object with a nonpositive weight (weight &lt;= 0) will not be selected.
+        /// Randomly select an item from <paramref name="list" /> using the weights in <paramref name="weights" /> given for each object. An object with a nonpositive weight (weight &lt;= 0) will not be selected.
         /// </summary>
         /// <remarks>
         /// The type for <paramref name="weights" /> should be changed from "int" to "INumerics" once Unity supports .NET 7 and generic math.
         /// </remarks>
-        /// <param name="objects">List of objects of type :<typeparamref name="T" />.</param>
+        /// <param name="list">List of items of type :<typeparamref name="T" />.</param>
         /// <param name="weights">Weights for each object.</param>
-        /// <typeparam name="T">Type of objects in <paramref name="objects" />.</typeparam>
-        /// <returns>Selected object from <paramref name="objects" /> or default(<typeparamref name="T" />) if <paramref name="objects" /> or <paramref name="weights" /> is null or empty, the list lengths do not match, or all objects have nonpositive weight (weight &lt;= 0).</returns>
-        public static T GetRandomFromWeightedList<T> (this IList<T> objects, IList<int> weights) {
-            if (objects == null || weights == null || objects.Count == 0 || weights.Count == 0 || objects.Count != weights.Count) return default(T);
-            if (objects.Count == 1) return (weights[0] > 0 ? objects[0] : default(T));
+        /// <typeparam name="T">Type of items in <paramref name="list" />.</typeparam>
+        /// <returns>Selected item from <paramref name="list" /> or default(<typeparamref name="T" />) if <paramref name="list" /> or <paramref name="weights" /> is null or empty, the list lengths do not match, or all items have nonpositive weight (weight &lt;= 0).</returns>
+        public static T GetRandomItemWeighted<T> (this IList<T> list, IList<int> weights) {
+            if (list == null || weights == null || list.Count == 0 || weights.Count == 0 || list.Count != weights.Count) return default(T);
+            if (list.Count == 1) return (weights[0] > 0 ? list[0] : default(T));
 
             int totalWeight = 0;
             for (int index = 0; index < weights.Count; index++) {
@@ -46,13 +44,13 @@ namespace GameCraftGuild.UnityExtensions.IList {
             int resultValue = Random.Range(0, totalWeight);
             int resultIndex = 0;
 
-            while (resultIndex < objects.Count && resultValue >= weights[resultIndex]) {
+            while (resultIndex < list.Count && resultValue >= weights[resultIndex]) {
                 resultValue -= weights[resultIndex];
                 resultIndex++;
             }
 
-            if (resultIndex >= objects.Count) return default(T);
-            return objects[resultIndex];
+            if (resultIndex >= list.Count) return default(T);
+            return list[resultIndex];
         }
     }
 }
