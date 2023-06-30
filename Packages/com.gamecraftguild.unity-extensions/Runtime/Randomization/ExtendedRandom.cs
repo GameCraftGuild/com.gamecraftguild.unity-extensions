6c2ff556da54d5afae36847ccdf678b09e1c928b
mitch <mitchkm21@gmail.com>
2023-06-30 14:24:55 -0700

    renames and removes prefixed "Random" on extended static random methods

--- Packages/com.gamecraftguild.unity-extensions/Runtime/Randomization/ExtendedRandom.cs
+++ Packages/com.gamecraftguild.unity-extensions/Runtime/Randomization/ExtendedRandom.cs
@@ -1,32 +1,32 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace GameCraftGuild.UnityExtensions.Randomization {
 
     /// <summary>
-    /// Class for generating random numbers.
+    /// Static class that uses Unity's Random number generator to more easily create Unity data structs with random values.
+    /// For example creating a random Vector2, Vector3, or Color.
     /// </summary>
-    public class Randomization {
+    public static class ExtendedRandom {
 
         /// <summary>
-        /// Return a Vector2 between the two Vector2s.
+        /// Return a Vector2 between the min and max values.
         /// </summary>
         /// <param name="min">Minimum values (inclusive).</param>
         /// <param name="max">Maximum values (inclusive).</param>
-        /// <returns>Created Vector2.</returns>
-        public static Vector2 RandomVector2 (Vector2 min, Vector2 max) {
+        /// <returns>A Vector2 with random values within constraints.</returns>
+        public static Vector2 Vector2 (Vector2 min, Vector2 max) {
             return new Vector2(Random.Range(min.x, max.x), Random.Range(min.y, max.y));
         }
 
         /// <summary>
         /// Return a Vector2 with values within the given ranges.
         /// </summary>
-        /// <param name="minX">Mimimum x value (inclusive).</param>
+        /// <param name="minX">Minimum x value (inclusive).</param>
         /// <param name="maxX">Maximum x value (inclusive).</param>
-        /// <param name="minY">Mimimum y value (inclusive).</param>
+        /// <param name="minY">Minimum y value (inclusive).</param>
         /// <param name="maxY">Maximum y value (inclusive).</param>
-        /// <returns>Created Vector2.</returns>
-        public static Vector2 RandomVector2 (float minX, float maxX, float minY, float maxY) {
+        /// <returns>A Vector2 with random values within constraints.</returns>
+        public static Vector2 Vector2 (float minX, float maxX, float minY, float maxY) {
             return new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
         }
 
@@ -36,7 +36,7 @@ namespace GameCraftGuild.UnityExtensions.Randomization {
         /// <param name="min">Min values for the color. RGB corresponds to XYZ. (inclusive)</param>
         /// <param name="max">Max values for the color. RGB corresponds to XYZ. (inclusive)</param>
         /// <returns>A random Color between the given values.</returns>
-        public static Color RandomColor (Vector3 min, Vector3 max) {
+        public static Color Color (Vector3 min, Vector3 max) {
             return new Color(Mathf.Clamp(Random.Range(min.x, max.x), 0, 1), Mathf.Clamp(Random.Range(min.y, max.y), 0, 1), Mathf.Clamp(Random.Range(min.z, max.z), 0, 1), 1f);
         }
 
@@ -50,7 +50,7 @@ namespace GameCraftGuild.UnityExtensions.Randomization {
         /// <param name="maxG">Max G value. (inclusive)</param>
         /// <param name="maxB">Max B value. (inclusive)</param>
         /// <returns>A random color between the given values.</returns>
-        public static Color RandomColor (float minR, float minG, float minB, float maxR, float maxG, float maxB) {
+        public static Color Color (float minR, float minG, float minB, float maxR, float maxG, float maxB) {
             return new Color(Mathf.Clamp(Random.Range(minR, maxR), 0, 1), Mathf.Clamp(Random.Range(minG, maxG), 0, 1), Mathf.Clamp(Random.Range(minB, maxB), 0, 1), 1f);
         }
 
@@ -60,7 +60,7 @@ namespace GameCraftGuild.UnityExtensions.Randomization {
         /// <param name="min">Min values for the color. RGBA corresponds to XYZW. (inclusive)</param>
         /// <param name="max">Max values for the color. RGBA corresponds to XYZW. (inclusive)</param>
         /// <returns>A random Color between the given values.</returns>
-        public static Color RandomColorAlpha (Vector4 min, Vector4 max) {
+        public static Color ColorAlpha (Vector4 min, Vector4 max) {
             return new Color(Mathf.Clamp(Random.Range(min.x, max.x), 0, 1), Mathf.Clamp(Random.Range(min.y, max.y), 0, 1), Mathf.Clamp(Random.Range(min.z, max.z), 0, 1), Mathf.Clamp(Random.Range(min.w, max.w), 0, 1));
         }
 
@@ -76,7 +76,7 @@ namespace GameCraftGuild.UnityExtensions.Randomization {
         /// <param name="maxB">Max B value. (inclusive)</param>
         /// <param name="maxA">Max A value. (inclusive)</param>
         /// <returns>A random Color between the given values.</returns>
-        public static Color RandomColorAlpha (float minR, float minG, float minB, float minA, float maxR, float maxG, float maxB, float maxA) {
+        public static Color ColorAlpha (float minR, float minG, float minB, float minA, float maxR, float maxG, float maxB, float maxA) {
             return new Color(Mathf.Clamp(Random.Range(minR, maxR), 0, 1), Mathf.Clamp(Random.Range(minG, maxG), 0, 1), Mathf.Clamp(Random.Range(minB, maxB), 0, 1), Mathf.Clamp(Random.Range(minA, maxA), 0, 1));
         }
 
@@ -86,7 +86,7 @@ namespace GameCraftGuild.UnityExtensions.Randomization {
         /// <param name="min">Min value. (inclusive)</param>
         /// <param name="max">Max value. (inclusive)</param>
         /// <returns>A random greyscale Color between the given values.</returns>
-        public static Color RandomGreyscale (float min, float max) {
+        public static Color Greyscale (float min, float max) {
             float value = Mathf.Clamp(Random.Range(min, max), 0, 1);
             return new Color(value, value, value, 1f);
         }
@@ -99,7 +99,7 @@ namespace GameCraftGuild.UnityExtensions.Randomization {
         /// <param name="minAlpha">Min alpha value. (inclusive)</param>
         /// <param name="maxAlpha">Max alpha value. (inclusive)</param>
         /// <returns>A random greyscale color between the given values with an alpha between the given values.</returns>
-        public static Color RandomGreyscaleAlpha (float min, float max, float minAlpha, float maxAlpha) {
+        public static Color GreyscaleAlpha (float min, float max, float minAlpha, float maxAlpha) {
             float value = Mathf.Clamp(Random.Range(min, max), 0, 1);
             return new Color(value, value, value, Mathf.Clamp(Random.Range(minAlpha, maxAlpha), 0, 1));
         }
